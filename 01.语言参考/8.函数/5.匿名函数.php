aa87d5123ca5b6375c8bbd97ac4761525713c1cd
Koishi <huben2koishi@gmail.com>
2018-10-28 22:20:10 +0800

    update

--- 01.语言参考/8.函数/5.匿名函数.php
+++ 01.语言参考/8.函数/5.匿名函数.php
@@ -28,3 +28,64 @@ $fun2 = function () use (&$message) {
 $fun2();
 $message = 'world';
 $fun2();
+echo "<hr/>";
+
+// closures 和作用域
+class Cart {
+    const PRICE_BUTTER = 1.00;
+    const PRICE_MILK = 3.00;
+    const PRICE_EGGS = 6.95;
+    
+    protected $products = array();
+    
+    public function add($product, $quantity) {
+        $this->products[$product] = $quantity;
+    }
+    
+    public function getQuantity($product) {
+        return isset($this->products[$product]) ? $this->products[$product] : FALSE;
+    }
+    
+    public function getTotal($tax) {
+        $total = 0.00;
+        $callback = function ($quantity, $product) use ($tax, &$total) {
+            $pricePerItem = constant(__CLASS__ . "::PRICE_" . strtoupper($product));
+            $total += ($pricePerItem * $quantity) * ($tax + 1.0);
+        };
+        array_walk($this->products, $callback);
+        return round($total, 2);
+    }
+}
+
+$my_cart = new Cart();
+$my_cart->add('butter', 1);
+$my_cart->add('milk', 3);
+$my_cart->add('eggs', 6);
+
+echo $my_cart->getTotal(0.05);
+echo "<hr/>";
+
+//
+class Test {
+    public function testing() {
+        return function () {
+            var_dump($this);
+        };
+    }
+}
+
+$obj = new Test();
+$fun = $obj->testing();
+$fun();
+echo "<hr/>";
+
+// 静态匿名函数中使用 $this
+class foo{
+    function __construct() {
+        $fun = static function(){
+            var_dump($this);
+        };
+        $fun();
+    }
+}
+new foo();
---
+++ 01.语言参考/9.类与对象/1.基本概念.php
@@ -0,0 +1,9 @@
+<?php
+// class
+class simple_class{
+    public $var = 'a default value';
+    
+    public function display_var() {
+        echo $this->var;
+    }
+}
