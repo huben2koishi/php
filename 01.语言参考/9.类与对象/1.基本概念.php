5777a95d370dc32d8e166d672560f41f8b722731
Koishi <huben2koishi@gmail.com>
2018-10-29 22:44:29 +0800

    update

--- 01.语言参考/9.类与对象/1.基本概念.php
+++ 01.语言参考/9.类与对象/1.基本概念.php
@@ -1,9 +1,82 @@
 <?php
+
+
+
 // class
-class simple_class{
+class simple_class {
     public $var = 'a default value';
     
     public function display_var() {
         echo $this->var;
     }
 }
+
+// new 关键字
+$a = new simple_class();
+$class_name = 'simple_class';
+$b = new $class_name();
+
+echo get_class($a) == get_class($b);
+echo '<hr/>';
+
+// 对象赋值
+$instance = new simple_class();
+$assigned = $instance;
+$reference = &$instance;
+
+$instance->var = '$assigned will have this value';
+
+var_dump($instance);
+echo '<br/>';
+var_dump($assigned);
+echo '<br/>';
+var_dump($reference);
+echo '<br/>';
+
+$instance = null;
+
+var_dump($instance);
+echo '<br/>';
+var_dump($assigned);
+echo '<br/>';
+var_dump($reference);
+echo '<br/>';
+
+echo '<hr/>';
+
+// 创建新对象
+class Test {
+    public static function getNew() {
+        return new static;
+    }
+}
+
+$obj1 = new Test();
+$obj2 = new $obj1;
+var_dump($obj1 !== $obj2);
+
+$obj3 = Test::getNew();
+var_dump($obj3 instanceof Test);
+
+echo '<hr/>';
+
+// extends 继承
+class extend_class extends simple_class {
+    function display_var() {
+        echo "Extending class<br/>";
+        parent::display_var();
+    }
+}
+
+$extend = new extend_class();
+$extend->display_var();
+
+echo '<hr/>';
+
+// ::class
+//namespace NS;
+class class_name {
+    
+}
+
+echo class_name::class;
