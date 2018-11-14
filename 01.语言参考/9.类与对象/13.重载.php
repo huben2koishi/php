859a08faeea231cb18e8496fba40755bdefeb4d7
Koishi <huben2koishi@gmail.com>
2018-11-14 17:15:42 +0800

    update

--- 01.语言参考/9.类与对象/13.重载.php
+++ 01.语言参考/9.类与对象/13.重载.php
@@ -1,5 +1,7 @@
 <?php
 // 动态地 "创建" 类属性和方法
+
+// 属性重载
 // void __set(String $name, mixed $value) 为不可访问属性赋值
 // mixed __get(String $name) 读取不可访问属性
 // bool __isset(String $name) 对不可访问属性调用 isset() empty() 时调用
@@ -14,25 +16,46 @@ class PropertyTest {
         echo "set $name to $value<br/>";
         $this->data[$name] = $value;
     }
+
     public function __get($name) {
-        echo "getting $name<br/>";
-        if (array_key_exists($name,$this->data)){
+
+         "getting $name<br/>";
+        if (array_key_exists($name, $this->data)) {
             return $this->data[$name];
         }
         $trace = debug_backtrace();
-        trigger_error('Undefined property via __get() '.$name.' in '.$trace[0]['file'].' on line '.$trace[0]['line'],E_USER_NOTICE);
+        trigger_error('Undefined property via __get() ' . $name . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'], E_USER_NOTICE);
         return null;
     }
+
     public function __isset($name) {
         echo "Is $name set?<br/>";
         return isset($this->data[$name]);
     }
+
     public function __unset($name) {
         echo "Unset $name<br/>";
         unset($this->data[$name]);
     }
+
+    public function getHidden(): int {
+        return $this->hidden;
+    }
 }
+
 echo '<pre>';
 $obj = new PropertyTest();
 $obj->a = 1;
 echo "\$obj->a.<br/>";
+
+var_dump(isset($obj->a));
+unset($obj->a);
+var_dump(isset($obj->a));
+echo '<br/>';
+
+echo $obj->declared . '<br/>';
+
+echo $obj->getHidden() . '<br/>';
+echo $obj->hidden;
+
+// 方法重载
