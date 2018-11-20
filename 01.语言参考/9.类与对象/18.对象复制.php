1e547696358b0edf3e601bc127d7c48b83cab1b7
Koishi <huben2koishi@gmail.com>
2018-11-20 17:32:43 +0800

    update

--- 01.语言参考/9.类与对象/18.对象复制.php
+++ 01.语言参考/9.类与对象/18.对象复制.php
@@ -1,1 +1,33 @@
 <?php
+
+class SubObject {
+    static $instances = 0;
+    public $instance;
+
+    public function __construct() {
+        $this->instance = ++self::$instances;
+    }
+
+    public function __clone() {
+        $this->instance = ++self::$instances;
+    }
+}
+
+class MyCloneable {
+    public $obj1, $obj2;
+
+    public function __clone() {
+        $this->obj1 = clone $this->obj1;
+    }
+}
+
+$obj = new MyCloneable();
+$obj->obj1 = new SubObject();
+$obj->obj2 = new SubObject();
+
+$obj2 = clone $obj;
+
+echo "<pre>原始对象:<br/>";
+var_dump($obj) ;
+echo "<br/>克隆对象:<br/>";
+var_dump( $obj2) ;
---
+++ 01.语言参考/9.类与对象/19.对象比较.php
@@ -0,0 +1,48 @@
+<?php
+// 比较运算符 ==  如果两个对象的属性和属性值都相等，且是同一个类的实例，则相等
+// 全等运算符 === 两个对象变量一定要指向某个类的同一个实例才相等
+
+function bool2str($bool) {
+    if ($bool === true) {
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+
+function compareObjects($obj1, $obj2) {
+    echo 'o1 == o2 : ' . bool2str($obj1 == $obj2) . "<br/>";
+    echo 'o1 != o2 : ' . bool2str($obj1 != $obj2) . "<br/>";
+    echo 'o1 === o2 : ' . bool2str($obj1 === $obj2) . "<br/>";
+    echo 'o1 !== o2 : ' . bool2str($obj1 !== $obj2) . "<br/>";
+}
+
+class Flag {
+    public $flag;
+
+    function __construct($flag = true) {
+        $this->flag = $flag;
+    }
+}
+
+class OtherFlag {
+    public $flag;
+
+    function __construct($flag = true) {
+        $this->flag = $flag;
+    }
+}
+
+$obj1 = new Flag();
+$obj2 = new Flag();
+$obj3 = $obj1;
+$obj4 = new OtherFlag();
+
+echo "同一个类的实例<br/>";
+compareObjects($obj1, $obj2);
+
+echo "<br/>Two references to the same instance<br/>";
+compareObjects($obj1, $obj2);
+
+echo "<br/>Instances of two different classes<br/>";
+compareObjects($obj1, $obj3);
