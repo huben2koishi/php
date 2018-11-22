d3332fd4f477df67ae20b9ed0ce4209e43004415
Koishi <huben2koishi@gmail.com>
2018-11-22 23:27:42 +0800

    update

--- 01.语言参考/10.命名空间/2.定义命名空间.php
+++ 01.语言参考/10.命名空间/2.定义命名空间.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Koishi
- * Date: 2018/11/21
- * Time: 23:04
- */
+// 受命名空间影响的代码 类 traits 接口 函数 常量
+namespace MyProject;
+
+const CONNECT_OK = 1;
+class Connection {
+}
+
+function connect() {
+}
+
+// namespace 的声明语句必须在所有代码之前(除了定义源文件编码方式的declare语句)
+
+// 命名空间允许像目录一样嵌套
+namespace MyProject\Sub\Level;
+
+const CONNECT_OK = 1;
+class Connection {
+}
+
+function connect() {
+}
---
+++ 01.语言参考/10.命名空间/3.定义多个命名空间.php
@@ -0,0 +1,31 @@
+<?php
+// 一个 php 文件中允许定义多个命名空间
+
+namespace MyProject {
+    const CONNECT_OK = 1;
+    class Connection {
+        static function start() {
+            return 1;
+        }
+    }
+    function connect() {
+        return 1;
+    }
+}
+
+
+namespace AnotherProject {
+    const CONNECT_OK = 1;
+    class Connection {
+    }
+    function connect() {
+    }
+}
+
+// 全局代码
+namespace {
+    session_start();
+    $a = MyProject\connect();
+    echo MyProject\Connection::start();
+}
+
